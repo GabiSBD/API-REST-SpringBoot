13a4211160288374e3cbdd92c1503d6ab8e7170d
GabiSBD <gabrielcruz1292@gmail.com>
2022-10-21 14:02:29 +0200

    todo funciona. borrado de metodos comentados e imports inecesarios

--- src/main/java/com/example/Laptop/security/WebSecurityConfig.java
+++ src/main/java/com/example/Laptop/security/WebSecurityConfig.java
@@ -2,18 +2,15 @@ package com.example.Laptop.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
-import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
+
 
 
 @Configuration
@@ -38,17 +35,7 @@ public class WebSecurityConfig{
                 .build();
         return new InMemoryUserDetailsManager(user, admin);
     }
-    /*@Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/api/get/**").authenticated().anyRequest()
-                .hasRole("USER").and()
-                .authorizeRequests().antMatchers("/**").authenticated().anyRequest()
-                .hasRole("ADMIN")
-                .and()
-                .formLogin().permitAll()
-                ;
-        return http.build();
-    }*/
+
 @Bean
     public PasswordEncoder passwordEncoder(){
         return new Pbkdf2PasswordEncoder();
